c8280021ea7486d17cb162c492a7647537d7cb07
DSPIRL <jj.proofing240@passmail.com>
2026-04-28 10:26:06 -0400

    Added new mp4togif function

--- private_dot_shell/functions.sh
+++ private_dot_shell/functions.sh
@@ -11,3 +11,15 @@ ref_dirinfo2() {
     echo "File count:  $(find "$dir" -type f | wc -l)"
     echo "Total size: $(find "$dir" -type f -print0 | du --files0-from=- -ch | tail -n1)"
 }
+
+mp4togif() {
+    local input="$1"
+    local fps="${2:-15}"
+    local width="${3:-720}"
+    local dither="${4:-bayer}"
+    local output="${input%.mp4}.gif"
+
+    ffmpeg -i "$input" \
+        -vf "fps=${fps},scale=${width}:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=256[p];[s1][p]paletteuse=dither=${dither}" \
+        "$output"
+}
