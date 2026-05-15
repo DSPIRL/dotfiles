d8fc5374f8e66162807e758b079e4047aa1fc931
DSPIRL <jj.proofing240@passmail.com>
2026-05-15 18:17:45 -0400

    Updating nvim plugins

---
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,47 @@
+local function workspace_path()
+	return vim.fn.getcwd()
+end
+
+return {
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*",
+		ft = "markdown",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		cmd = {
+			"ObsidianBacklinks",
+			"ObsidianDailies",
+			"ObsidianFollowLink",
+			"ObsidianLink",
+			"ObsidianLinkNew",
+			"ObsidianLinks",
+			"ObsidianNew",
+			"ObsidianOpen",
+			"ObsidianPasteImg",
+			"ObsidianQuickSwitch",
+			"ObsidianSearch",
+			"ObsidianTags",
+			"ObsidianTemplate",
+			"ObsidianToday",
+			"ObsidianToggleCheckbox",
+			"ObsidianWorkspace",
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "notes",
+					path = workspace_path,
+				},
+			},
+			completion = {
+				nvim_cmp = false,
+				min_chars = 2,
+			},
+			picker = {
+				name = "telescope.nvim",
+			},
+		},
+	},
+}
