80427a42d99f47a65b1b166ace33bc8665697c9b
DSPIRL <jj.proofing240@passmail.com>
2026-06-18 15:36:52 -0400

    NVIM updates

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,4 +3,16 @@
 -- Add any additional keymaps here
 
 -- USER EDITS START --
+Snacks.toggle({
+	name = "Completion Auto Popup",
+	get = function()
+		return vim.g.completion_auto_show ~= false
+	end,
+	set = function(state)
+		vim.g.completion_auto_show = state
+		if not state and package.loaded["blink.cmp"] then
+			require("blink.cmp").hide()
+		end
+	end,
+}):map("<leader>uE")
 -- USER EDITS END --
