3a122e6a3f25b5689d50bf16aa1a9d1ce46705ee
DSPIRL <jj.proofing240@passmail.com>
2026-06-19 17:24:27 -0400

    making hyprland changes

--- dot_config/hypr/scripts/executable_rofi-emoji-picker.sh
+++ dot_config/hypr/scripts/executable_rofi-emoji-picker.sh
@@ -4,5 +4,5 @@ if pgrep -x "rofi" >/dev/null; then
     pkill -x "rofi" && pkill -x "rofimoji"
 else
     rofi_theme="$HOME/.config/rofi/config-emoji.rasi"
-    rofimoji --max-recent 5 --hidden-descriptions --selector-args="-config $rofi_theme -kb-row-left Left -kb-row-right Right -kb-move-char-back Control+b -kb-move-char-forward Control+f" --action copy
+    rofimoji --max-recent 0 --hidden-descriptions --selector-args="-config $rofi_theme -kb-row-left Left -kb-row-right Right -kb-move-char-back Control+b -kb-move-char-forward Control+f" --action copy
 fi
